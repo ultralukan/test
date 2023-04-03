56dc60d81534ffe3795e2b0035ea403419c41218
ultralukan <ultralukan@mail.ru>
2023-04-03 13:31:47 +0300

    update 9

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,27 +13,29 @@ export default function About() {
           <div className={styles.about_level}>
             <h3>Повышаем уровень обслуживания</h3>
             <div className={styles.about_text_dots}>&#x2022; &#x2022; &#x2022;</div>
-            <div>Наша компания существует для того, чтобы улучшить сервис Вашего заведения. Мы понимаем, что качество обслуживания - это трудоемкая и щепетильная работа, поэтому мы стараемся ее упростить. Делаем это блягодаря современным технологиям. Устанавливаем БЗУ - Беспроводное зарядное устройство и планшеты с электронным меню, не прибегая к изменениям интерьера. </div>
-          </div>
-          <div className={styles.about_qualities}>
-            <div className={styles.about_card}>
-              <div className={styles.about_icon}>
-                <img src='hands.svg'/>
-              </div>
-              <h3>Золотые руки</h3>
-              <div className={styles.about_card_text}>Вся работа будет выполнена профессионалами. А так же эксперт предложит несколько вариантов установки.</div>
-            </div>
-            <div className={styles.about_card}>
-              <div className={styles.about_icon}>
-                <img src='medal.svg'/>
-              </div>
-              <h3>Деликатность<br/>и красота</h3>
-              <div className={styles.about_card_text}>Аккуратный монтаж. Интерьер не меняется. Перестановка мебели не производится, все остается на своих местах. Появится только маленькая, информирующая метка на столе.</div>
+            <div className={styles.text}>Improvement Service существует для того, чтобы улучшить сервис Вашего заведения. Мы понимаем, что качество обслуживания - это трудоемкий процесс, поэтому мы стараемся его упростить. Наша компания устанавливает БЗУ - беспроводное зарядное устройство, а также планшеты с электронным меню, без деформации интерьера Вашего заведения.</div>
+            <div className={styles.btn}>
+              <Button/>
             </div>
           </div>
+          {/*<div className={styles.about_qualities}>*/}
+          {/*  <div className={styles.about_card}>*/}
+          {/*    <div className={styles.about_icon}>*/}
+          {/*      <img src='hands.svg'/>*/}
+          {/*    </div>*/}
+          {/*    <h3>Золотые руки</h3>*/}
+          {/*    <div className={styles.about_card_text}>Вся работа будет выполнена профессионалами. А так же эксперт предложит несколько вариантов установки.</div>*/}
+          {/*  </div>*/}
+          {/*  <div className={styles.about_card}>*/}
+          {/*    <div className={styles.about_icon}>*/}
+          {/*      <img src='medal.svg'/>*/}
+          {/*    </div>*/}
+          {/*    <h3>Деликатность<br/>и красота</h3>*/}
+          {/*    <div className={styles.about_card_text}>Аккуратный монтаж. Интерьер не меняется. Перестановка мебели не производится, все остается на своих местах. Появится только маленькая, информирующая метка на столе.</div>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
         </div>
       </div>
-      <Button/>
     </div>
   )
 }
